2be49290d3ef487c976405ea10887bf0e156d22a
Gauthier FONTAINE <gauthier.fontaine@gmail.com>
2022-06-22 19:21:51 +0200

    Import police "Open Sans"
    Chargement de la police "Open Sans" depuis Google Fonts.
    
    A changer pour la prod

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -257,6 +257,9 @@ export default {
 </script>
 
 <style lang="scss">
+// Imports
+// Police "Open Sans"
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,700;1,400&display=swap');
 // Variables
     // Polices
     $primary_font: "Open Sans", sans-serif;
